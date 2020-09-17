85a285beb2c04f85d39233b418760d3333469f92
StarsX <X-Universe@outlook.com>
2020-09-17 09:24:52 +0800

    Update CSTemporalAA.hlsl

--- IrradianceMap/Content/Shaders/CSTemporalAA.hlsl
+++ IrradianceMap/Content/Shaders/CSTemporalAA.hlsl
@@ -30,27 +30,39 @@ static const int2 g_texOffsets[] =
 //--------------------------------------------------------------------------------------
 // Texture and buffers
 //--------------------------------------------------------------------------------------
+#ifdef _R11G11B10_
+RWTexture2D<float3>	g_rwRenderTarget;
+RWTexture2D<float>	g_rwMetaData;
+Texture2D<float3>	g_txCurrent;
+Texture2D<float3>	g_txHistory;
+Texture2D<float2>	g_txVelocity;
+Texture2D<float>	g_txMasks;
+Texture2D<float>	g_txHistMeta;
+#else
 RWTexture2D<float4>	g_rwRenderTarget;
-Texture2D			g_txCurrent		: register (t0);
-Texture2D			g_txHistory		: register (t1);
-Texture2D<float2>	g_txVelocity	: register (t2);
+Texture2D			g_txCurrent;
+Texture2D			g_txHistory;
+Texture2D<float2>	g_txVelocity;
+#endif
 
 //--------------------------------------------------------------------------------------
 // Sampler
 //--------------------------------------------------------------------------------------
 SamplerState g_smpLinear;
 
-// --------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------
 // A fast invertible tone map that preserves color (Reinhard)
-// --------------------------------------------------------------------------------------
-min16float3 TM(min16float3 rgb)
+//--------------------------------------------------------------------------------------
+min16float3 TM(float3 hdr)
 {
+	const min16float3 rgb = min16float3(hdr);
+
 	return rgb / (1.0 + dot(rgb, g_lumBase));
 }
 
-// --------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------
 // Inverse of preceding function
-// --------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------
 min16float3 ITM(min16float3 rgb)
 {
 	return rgb / max(1.0 - dot(rgb, g_lumBase), 1e-4);
@@ -59,14 +71,14 @@ min16float3 ITM(min16float3 rgb)
 //--------------------------------------------------------------------------------------
 // Maxinum velocity of 3x3
 //--------------------------------------------------------------------------------------
-min16float4 VelocityMax(int2 tex)
+min16float4 VelocityMax(int2 pos)
 {
-	const float2 velocity = g_txVelocity[tex];
+	const float2 velocity = g_txVelocity[pos];
 
 	float2 velocities[NUM_NEIGHBORS_H];
 	[unroll]
 	for (uint i = 0; i < NUM_NEIGHBORS_H; ++i)
-		velocities[i] = g_txVelocity[tex + g_texOffsets[i + NUM_NEIGHBORS_H]];
+		velocities[i] = g_txVelocity[pos + g_texOffsets[i + NUM_NEIGHBORS_H]];
 
 	min16float4 velocityMax = min16float2(velocity).xyxy;
 	min16float speedSq = dot(velocityMax.xy, velocityMax.xy);
@@ -74,12 +86,16 @@ min16float4 VelocityMax(int2 tex)
 	for (i = 0; i < NUM_NEIGHBORS_H; ++i)
 	{
 		const min16float2 neighbor = min16float2(velocities[i]);
+#if 0
+		velocityMax.xy = max(neighbor, velocityMax.xy);
+#else
 		const min16float speedSqN = dot(neighbor, neighbor);
 		if (speedSqN > speedSq)
 		{
 			velocityMax.xy = neighbor;
 			speedSq = speedSqN;
 		}
+#endif
 	}
 
 	return velocityMax;
@@ -168,58 +184,61 @@ void main(uint2 DTid : SV_DispatchThreadID)
 {
 	float2 texSize;
 	g_txHistory.GetDimensions(texSize.x, texSize.y);
-	const float2 tex = (DTid + 0.5) / texSize;
+	const float2 uv = (DTid + 0.5) / texSize;
 
 	// Load G-buffers
 	const float4 current = g_txCurrent[DTid];
 	const min16float4 velocity = VelocityMax(DTid);
-	const float2 texBack = tex - velocity.xy;
-	float4 history = g_txHistory.SampleLevel(g_smpLinear, texBack, 0);
+	const float2 uvBack = uv - velocity.xy;
+	float4 history = g_txHistory.SampleLevel(g_smpLinear, uvBack, 0);
 
 	// Speed to history blur
 	const float2 historyBlurAmp = 4.0 * texSize;
 	const min16float2 historyBlurs = min16float2(abs(velocity.xy) * historyBlurAmp);
-	min16float historyBlurMin = saturate(historyBlurs.x + historyBlurs.y);
+	min16float curHistoryBlur = saturate(historyBlurs.x + historyBlurs.y);
 
-	// Mask and historical mask
+	// Evaluate history weight that indicates the convergence from metadata
 	min16float historyBlur = min16float(1.0 - history.w);
-	historyBlur = max(historyBlur, historyBlurMin);
+	historyBlur = max(historyBlur, curHistoryBlur);
 	history.w = history.w * g_historyMax + 1.0;
 
+	// Compute color-space AABB
 	min16float4 neighborMin, neighborMax;
-	min16float4 curColor = min16float4(current);
-	curColor.xyz = TM(curColor.xyz);
+	const min16float4 currentTM = min16float4(TM(current.xyz), current.w);
 	const min16float gamma = historyBlur > 0.0 || current.w <= 0.0 ? 1.0 : 16.0;
-	min16float4 filtered = NeighborMinMax(neighborMin, neighborMax, curColor, DTid, gamma);
+	min16float4 filtered = NeighborMinMax(neighborMin, neighborMax, currentTM, DTid, gamma);
 	
-	min16float3 histColor = TM(min16float3(history.xyz));
-	histColor = clipColor(histColor, neighborMin.xyz, neighborMax.xyz);
+	// Clip historical color
+	min16float3 historyTM = TM(min16float3(history.xyz));
+	historyTM = clipColor(historyTM, neighborMin.xyz, neighborMax.xyz);
 	const min16float contrast = neighborMax.w - neighborMin.w;
 
 	// Add aliasing
 	static const min16float lumContrastFactor = 32.0;
 	min16float addAlias = historyBlur * 0.5 + 0.25;
 	addAlias = saturate(addAlias + 1.0 / (1.0 + contrast * lumContrastFactor));
-	filtered.xyz = lerp(filtered.xyz, curColor.xyz, addAlias);
+	filtered.xyz = lerp(filtered.xyz, currentTM.xyz, addAlias);
 
-	// Calculate Blend factor
-	const min16float lumHist = GET_LUMA(histColor);
+	// Calculate blend factor
+	const min16float lumHist = GET_LUMA(historyTM);
 	const min16float distToClamp = min(abs(neighborMin.w - lumHist), abs(neighborMax.w - lumHist));
 #if 0
 	const float historyAmt = 1.0 / history.w + historyBlur / 8.0;
 	const min16float historyFactor = min16float(distToClamp * historyAmt * (1.0 + historyBlur * historyAmt * 8.0));
 	min16float blend = historyFactor / (distToClamp + contrast);
-	blend = historyBlur > 0.0 && current.w > 0.0 ? blend : min16float(historyAmt);
-	blend = min(blend, 0.25);
 #else
-	const float historyFactor = min(distToClamp - contrast, 0.0);
-	min16float blend = min16float(1.0 / history.w + historyBlurMin * historyFactor);
-	blend = historyBlur > 0.0 || filtered.w < 1.0 ? blend : 0.0;
-	blend = clamp(blend, 0.0, 0.2);
+	const min16float historyAmt = min(min16float(1.0 / history.w + historyBlur / 8.0), 1.0);
+	min16float blend = 0.0625 / lerp(2.0, distToClamp + contrast, historyAmt);
 #endif
+	blend = min(blend, 0.25);
 
-	const min16float3 result = ITM(lerp(histColor, filtered.xyz, blend));
-	history.w = min(history.w / g_historyMax, max(1.0 - historyBlurMin, 0.0));
+	const min16float3 result = ITM(lerp(historyTM, filtered.xyz, blend));
+	history.w = min(history.w / g_historyMax, max(1.0 - curHistoryBlur, 0.0));
 
+#ifdef _R11G11B10_
+	g_rwRenderTarget[DTid] = result;
+	g_rwMetaData[DTid] = history.w;
+#else
 	g_rwRenderTarget[DTid] = float4(result, history.w);
+#endif
 }
