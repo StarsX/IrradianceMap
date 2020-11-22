13dc996d0e504245027b8921842fb9d4ee52602a
StarsX <X-Universe@outlook.com>
2020-11-22 17:20:47 +0800

    Remove redundant headers.

--- IrradianceMap/stdafx.h
+++ IrradianceMap/stdafx.h
@@ -32,14 +32,17 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
-#include <codecvt>
 #include <iomanip>
 
+#if _HAS_CXX17
+#include <winrt/base.h>
+#else
 #include <algorithm>
 #include <string>
 #include <vector>
 #include <map>
 #include <unordered_map>
+#endif
 #include <functional>
 #include <wrl.h>
 #include <shellapi.h>
