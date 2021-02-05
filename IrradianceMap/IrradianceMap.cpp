c43e5da3c27efa15d1f3c6cdc66e3eab05712040
StarsX <X-Universe@outlook.com>
2021-02-05 10:11:01 +0800

    XUSG updated for uploader fix.

--- IrradianceMap/IrradianceMap.cpp
+++ IrradianceMap/IrradianceMap.cpp
@@ -286,10 +286,10 @@ void IrradianceMap::OnKeyUp(uint8_t key)
 {
 	switch (key)
 	{
-	case 0x20:	// case VK_SPACE:
+	case VK_SPACE:
 		m_isPaused = !m_isPaused;
 		break;
-	case 0x70:	//case VK_F1:
+	case VK_F1:
 		m_showFPS = !m_showFPS;
 		break;
 	case 'G':
