d79d78240c63e27e70be4fcc88e34855f82b977d
StarsX <X-Universe@outlook.com>
2023-10-05 12:35:22 +0800

    Camera up direction should be a vector

--- IrradianceMap/IrradianceMap.cpp
+++ IrradianceMap/IrradianceMap.cpp
@@ -210,7 +210,7 @@ void IrradianceMap::LoadAssets()
 	m_eyePt = XMFLOAT3(4.0f, 6.0f, -20.0f);
 	const auto focusPt = XMLoadFloat3(&m_focusPt);
 	const auto eyePt = XMLoadFloat3(&m_eyePt);
-	const auto view = XMMatrixLookAtLH(eyePt, focusPt, XMVectorSet(0.0f, 1.0f, 0.0f, 1.0f));
+	const auto view = XMMatrixLookAtLH(eyePt, focusPt, XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f));
 	XMStoreFloat4x4(&m_view, view);
 }
 
